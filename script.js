8e0ac36629b9d02ee6ecdf3da115fb13b922c395
Anna <Anna@Annas-Air.attlocal.net>
2023-07-06 11:15:54 -0500

    added to initial files, currently not working

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-// Write your JavaScript code here!
+console.log("aaaa")
+// const scriptHelper = require("./scriptHelper") 
+// const validateInput = scriptHelper.validateInput()
+// const myFetch = scriptHelper.myFetch()
+
+
+
+
 
 window.addEventListener("load", function() {
+  
+//    let listedPlanets;
+//    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+//    let listedPlanetsResponse;
+ 
+//    listedPlanetsResponse.then(function (result) {
+//        listedPlanets = result;
+//        console.log(listedPlanets);
+      
+//    }).then(function () {
+//        console.log(listedPlanets);
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+//        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+//    })
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,27 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+
+// require('isomorphic-fetch');
+
+window.addEventListener("load", function(){
+    alert("tdyujdhfg")
+    let form = document.querySelector("#formSubmit")
+    this.alert("ggggggg")
+    form.addEventListener("submit", function(event){
+        window.preventDefault();
+        alert("aaaaaaa")
+
+        let chosenPilot = document.querySelector("input[name = pilotName]")
+        let chosenCopilot = document.querySelector("input[name = copilotName]")
+        let chosenFuelLevel = document.querySelector("input [name = fuelLevel]")
+        let chosenCargoMass = document.querySelector("input[name = cargoMass]")
+
+        let options = [chosenPilot, chosenCopilot, chosenFuelLevel, chosenCargoMass]
+        //pass to formSubmission
+        alert("SDFGHJKL")
+    
+
+    })
+})
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
@@ -15,13 +37,38 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 <img src="">
    */
 }
-
+console.log(2)
 function validateInput(testInput) {
-   
+alert(1)
+    
+            //pass to formSubmission
+
+            if (options.includes("")){
+                alert("All fields are required!")
+                event.preventDefault()
+            }else{
+                alert(">:(")
+            }
+
+
+        }
+    
+
+    // pilotName, copilotName, fuelLevel, cargoMass
+
+    //The pilot and co-pilot names should be strings and the fuel level and cargo mass should be numbers
+    //validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate
 }
 
+
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+   //put load listener here
+   //call validateInput and pass info to it
+   //
    
+   
+    //use validateInput() to complete the formSubmission() function. formSubmission() will take in a document parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass. 
+    //Using the values in those strings and the document parameter for your HTML document, update the shuttle requirements as described below. Make sure to call your formSubmission() function at the appropriate time in your script.js file!
 }
 
 async function myFetch() {
