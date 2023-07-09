c1aee758604bf3f36ee676419849cadc24b7f066
Anna <Anna@Annas-Air.attlocal.net>
2023-07-09 01:23:44 -0500

    Added interactive status

--- scriptHelper.js
+++ scriptHelper.js
@@ -50,22 +50,22 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
     function getValue(variable){ return variable.value} // get values from variables
 
+
+
+
     function formSubmission(document, pilot, copilot, fuelLevel, cargoMass) {
         
-       
-
-        let chosenPilot = document.querySelector("input[name = " + pilot + "]")
-        let chosenCopilot = document.querySelector("input[name = " + copilot + "]")
-        let chosenFuelLevel = document.querySelector("input[name = " + fuelLevel + "]")
-        let chosenCargoMass = document.querySelector("input[name = " + cargoMass + "]")
+        let chosenPilot = document.querySelector(`input[name = ${pilot}]`)
+        let chosenCopilot = document.querySelector(`input[name = ${copilot}]`)
+        let chosenFuelLevel = document.querySelector(`input[name = ${fuelLevel}]`)
+        let chosenCargoMass = document.querySelector(`input[name = ${cargoMass}]`)
 
         let options = [chosenPilot, chosenCopilot, chosenFuelLevel, chosenCargoMass] //array of options
-        let optionsValues = options.map(getValue) //get values for options
+        let optionsValues = options.map(getValue) //get string values for options w/ getValue Function (validate input uses the string value, not the variable itself)
 
         let optionsReturns = optionsValues.map(validateInput) //pass to validateInput, get array of responses
         
         function alertsCheck(){ 
-            
             if (optionsReturns.includes("Empty")){
                 alert("All fields are required.")
             }else if (optionsReturns[0]==="Is a Number"){ //pilot and co-pilot names should be strings
@@ -79,18 +79,57 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
             }else{
                 return //if no alerts are tripped, return before preventDefault()
             }
-            preventDefault()
+            event.preventDefault()
         }
         alertsCheck()
         
         
+       
+
+        function updateStatus(){
+            pilotStatus.innerHTML = `Pilot ${optionsValues[0]} is Ready`    //update pilot status with selected pilots
+            copilotStatus.innerHTML = `Co-pilot ${optionsValues[1]} is Ready`
+
+        
+            let fuelReady = false
+            let cargoReady = false
+
+            if(Number(optionsValues[2]) < 10000){                           //fuelStatus readyness
+                fuelStatus.innerHTML = "There is not enough fuel for the journey."
+                fuelReady = false
+            } else{
+                fuelStatus.innerHTML = "Fuel level high enough for launch."
+                fuelReady = true
+            }
+            
+            if(Number(optionsValues[3]) > 10000){                           //cargoStatus readyness
+                cargoStatus.innerHTML = "There is too much mass for the shuttle to take off."
+                cargoReady = false
+            } else{
+                cargoStatus.innerHTML = "Cargo mass low enough for launch."
+                cargoReady = true
+            }
+            
+            
+            if (fuelReady === true && cargoReady === true){                 //if shuttle is ready 
+                launchStatus.innerHTML = "Shuttle is ready for launch."
+                launchStatus.style.color = "green"
+                faultyItems.style.visibility = "hidden"
+            } else {                                                         //if shuttle isn't ready
+                launchStatus.innerHTML = "Shuttle not ready for launch."
+                launchStatus.style.color = "red"
+                faultyItems.style.visibility = "visible"
+            }
+        }
+        updateStatus()
         
 
-   
 
-    
-        //formSubmission() will take in a document parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass. 
-        //Using the values in those strings and the document parameter for your HTML document, update the shuttle requirements as described below. Make sure to call your formSubmission() function at the appropriate time in your script.js file!
+
+
+
+
+        
     }
 
 
