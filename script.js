f1197e1f18a015be3ecbf6bccccd71d9140f2d25
Anna <Anna@Annas-Air.attlocal.net>
2023-07-09 18:36:41 -0500

    a

--- script.js
+++ script.js
@@ -4,12 +4,10 @@
 
 window.addEventListener("load", function() {
 
-    let form = document.querySelector("form")
+    let form = document.querySelector("form") //button, call formSubmission
     form.addEventListener("submit", function(event){
         event.preventDefault();
         formSubmission(document, "pilotName", "copilotName", "fuelLevel", "cargoMass")
-     
-
     })
 
     
@@ -25,26 +23,5 @@ window.addEventListener("load", function() {
         addDestinationInfo(pickPlanet(listedPlanets))
 
     })
-
-
-
-
-
-
-
-  
-//    let listedPlanets;
-//    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-//    let listedPlanetsResponse;
- 
-//    listedPlanetsResponse.then(function (result) {
-//        listedPlanets = result;
-//        console.log(listedPlanets);
-      
-//    }).then(function () {
-//        console.log(listedPlanets);
-
-//        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-//    })
    
 });
